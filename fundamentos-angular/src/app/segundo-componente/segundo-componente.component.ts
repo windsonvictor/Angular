9145801a8f6c0e8da2fc1f336fdab3fe20d8f8a8
windsonvictor <103837062+windsonvictor@users.noreply.github.com>
2022-10-03 21:16:16 -0300

    aula 4-d

--- fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
+++ fundamentos-angular/src/app/segundo-componente/segundo-componente.component.ts
@@ -9,5 +9,9 @@ export class SegundoComponenteComponent  {
 
     nome="Windson";
     dataNascimento="2000-11-08";
+    urlImagem="/assets/avataaars2.png";
 
+    mostrarDataNascimento(){
+  alert(`A data de nascimento de Windson é:${this.dataNascimento} `);
+    }
 }
