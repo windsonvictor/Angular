571af9ac2c68333f51104e47f1b772a4dd481378
windsonvictor <103837062+windsonvictor@users.noreply.github.com>
2022-10-18 11:19:40 -0300

    aual 6f

--- proway-computers/src/app/app-routing.module.ts
+++ proway-computers/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'produtos', loadChildren: () => import('./produtos/produtos.module').then(m => m.ProdutosModule) },
+{path:"",redirectTo:"produtos",pathMatch:"full"},
+{path:"**", component:NaoEncontradaComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- proway-computers/src/app/app.module.ts
+++ proway-computers/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NaoEncontradaComponent } from './nao-encontrada/nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    NaoEncontradaComponent
   ],
   imports: [
     BrowserModule,
